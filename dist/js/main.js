e9ee553ff70248d708dcaef6b905254076db50d5
Jeremy Southon <southon55@gmail.com>
2019-04-15 13:39:55 +1200

    added project modals

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,9 +7,9 @@ const navItems = document.querySelectorAll('.nav-item');
 const filters = document.querySelectorAll('.filter');
 const projects = document.querySelectorAll('.item');
 
-setTimeout(()=> {
+setTimeout(() => {
     navItems.forEach(item => item.classList.add('showing'))
-},0.4)
+}, 0.4)
 
 let menuOpen = false;
 let currentFilter = "all"; // for filtering projects
@@ -72,6 +72,125 @@ function handleProjectFilter() {
     });
 }
 
+/**
+ * Code for project modals.
+ */
+let imageIndex = 0;
+let primaryModalImage; // image
+let modalImages; // current modal showing
+/**
+ * function for reseting some of the variables used for the model.
+ * called when a modal is opened. 
+ * 
+ * used for prev and next options for modal.
+ */
+function openModel(modelID) {
+    modalImages = document.querySelectorAll("." + modelID + "_projectImage");
+    imageIndex = 0;
+    primaryModalImage = document.getElementById(`${modelID + '_main_img'}`)
+
+}
+
+/**
+ * handles logic for pressing the previous image button in modal view
+ * @param {string} modelID 
+ */
+function prevImage(modelID) {
+    imageIndex--;
+    if (imageIndex < 0) {
+        imageIndex = modalImages.length - 1;
+    }
+    //unshow current showing
+    removeShowingClass(modalImages);
+    //update new showing image.
+    modalImages[imageIndex].classList.add('showing');
+}
+
+function nextImage(modelID) {
+    imageIndex++;
+    if (imageIndex > modalImages.length - 1) {
+        imageIndex = 0;
+    }
+    //unshow current showing
+    removeShowingClass(modalImages);
+    //update new showing image.
+    modalImages[imageIndex].classList.add('showing');
+}
 
 
-// code responsible for triggering the pop up
+/**
+ * Helper function for removing the current showing modal image.
+ * @param {NodeList of <img>} htmlImageList 
+ */
+const removeShowingClass = (htmlImageList) => {
+    htmlImageList.forEach(img => {
+        if (img.classList.contains('showing')) {
+            img.classList.remove('showing');
+        }
+    })
+}
+
+/**
+ * Called once a model X is pressed, will set visible to hidden;
+ * @param {string} modelID 
+ */
+function closeModal() {
+    console.log('close modal')
+    const modalDiv = document.getElementById('modalContainer');
+    modalDiv.style.visibility = 'hidden';
+}
+
+
+/**
+ * One thumbnail is clicked, set modal to visible.
+ * @param {string} modelID 
+ */
+function showModal(modelID) {
+    openModel(modelID);
+    const modalDiv = document.getElementById('modalContainer');
+    modalDiv.style.visibility = 'visible';
+    
+}
+
+function createModal(title, images) {
+
+    const projectImages = images.split(',');
+    let projImgs = document.createElement('div');
+    for (let i = 0; i < projectImages.length; i++) {
+        let img = projectImages[i];
+        let htmlImage = document.createElement('img');
+        // all project images will have their image, the project thats its associated with and 
+        // a class of prj
+        htmlImage.setAttribute('src', `../../dist/img/projects/${img}`)
+        htmlImage.classList.add(`${title}_projectImage`);
+        htmlImage.classList.add('prjImg');
+
+        if (i === 0) {
+            //first image is main
+            htmlImage.classList.add('showing');
+            htmlImage.id = `${title}_main_img`;
+        }
+        projImgs.appendChild(htmlImage)
+    }
+
+    console.log(projImgs.innerHTML)
+    const modelContainer = document.getElementById('modalContainer');
+    modelContainer.innerHTML =
+        `
+    <div class="light-modal-content animated slideInDown">
+        <div class="light-modal-header">
+            <h3 class="light-modal-heading">${title}</h3>
+            <a class="light-modal-close-icon" onclick="closeModal()" aria-label="close">X</a>
+        </div>
+        ${projImgs.innerHTML}
+
+        <div class="light-modal-navigation">
+            <a onclick="prevImage('${title}')" class="light-modal-navigation navigation-prev"></a>
+            <a onclick="nextImage('${title}')" class="light-modal-navigation navigation-next"></a>
+        </div>
+    </div>
+`
+    showModal(title)
+    modelContainer.style.visibility = 'visible'
+
+}
